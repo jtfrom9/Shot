33bbc49dc085511f49dce72727f5686f1aee7e9e
Jun Tachikawa <jtachikawa@xevo.com>
2020-06-08 20:18:53 +0900

    add 3 targets in a new wave

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,19 +8,34 @@ public class GameManager : MonoBehaviour
     public GameObject bulletPrefab;
     public GameObject targetPrefab;
     public Text scoreText;
+    public Text waveText;
 
     int score = 0;
+    int wave = 1;
 
-    void Start()
+    void Generate()
     {
-        for (int i = 0; i < 3; i++) {
+        waveText.text = $"Wave: {wave}";
+        for (int i = 0; i < 3; i++)
+        {
             var pos = new Vector3(Random.Range(-45, 45),
                 Random.Range(1, 25),
                 Random.Range(30, 50));
-            Instantiate(targetPrefab, 
-                Camera.main.transform.position + pos, 
+            Instantiate(targetPrefab,
+                Camera.main.transform.position + pos,
                 Quaternion.identity);
         }
+
+        var next = 8.0f - wave * 0.3f;
+        if(next < 1.0f) next = 1.0f;
+        Invoke("Generate", next);
+
+        wave++;
+    }
+
+    void Start()
+    {
+        Invoke("Generate", 1);
     }
 
     void Update()
@@ -34,7 +49,7 @@ public class GameManager : MonoBehaviour
             Debug.Log($"screen: {pos}, dir: {ray.direction}");
 
             bullet.OnHit += () => {
-                score++;
+                score += wave;
                 scoreText.text = $"Score: {score}";
             };
             bullet.Shot(ray.direction.normalized * 3000);
